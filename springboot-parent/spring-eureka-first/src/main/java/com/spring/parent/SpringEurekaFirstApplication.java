5d0e14436bfa76c73d3f4b9c181e0db3ab4dc400
yijiang.ding <782457114@qq.com>
2019-08-07 10:21:52 +0800

    maven 多模块 springBoot+springCloud  eureka 初步集成

---
+++ springboot-parent/spring-eureka-first/src/main/java/com/spring/parent/SpringEurekaFirstApplication.java
@@ -0,0 +1,13 @@
+package com.spring.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEurekaFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaFirstApplication.class, args);
+	}
+
+}
