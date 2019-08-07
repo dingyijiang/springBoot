78b2858e23f93d24e44fb84526279b607abba98c
yijiang.ding <782457114@qq.com>
2019-08-07 18:28:51 +0800

    2019-08-07

--- springboot-parent/spring-eureka-second/src/main/java/com/spring/parent/SpringEurekaSecondApplication.java
+++ springboot-parent/spring-eureka-second/src/main/java/com/spring/parent/SpringEurekaSecondApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SpringEurekaFirstApplication {
+public class SpringEurekaSecondApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringEurekaFirstApplication.class, args);
+		SpringApplication.run(SpringEurekaSecondApplication.class, args);
 	}
 
 }
